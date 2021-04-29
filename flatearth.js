362a853e564ebd631dce59cd70bb771af4dd9e19
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-04-29 11:05:45 +0200

    wrangle maps

--- flatearth.js
+++ flatearth.js
@@ -2,7 +2,7 @@ var map = L.map('map', {
    	crs: L.CRS.Simple
    });
    var bounds = [[0,0], [1200,1200]];
-   var image = L.imageOverlay('maps/airagemap.png', bounds).addTo(map);
+   var image = L.imageOverlay('maps/air_age_bigger.jpg', bounds).addTo(map);
 // on localhost much bigger files work
 map.fitBounds(bounds);
 var marker = L.marker(new L.LatLng(592, 748), {
