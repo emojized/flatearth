1a4a7c6aaafd24c53e5fbe8ecc758f7ec0ae72df
emojized <maze@theo-dege.net>
2023-10-09 22:51:42 +0200

    adding the draw tool to leaflet, TODO: has to be persistent until browser close

--- map.js
+++ map.js
@@ -1,25 +1,33 @@
 // map
 
 
-   let bounds = [[0,0], [1200,1200]];
-   let image1 = L.imageOverlay('maps/air_age_bigger.jpg', bounds);
-   
-   let image2 = L.imageOverlay('maps/flat_earth_xxl.jpg', bounds);
-   
-   let map1Group = L.layerGroup([image1]);
-   let map2Group = L.layerGroup([image2]);
+let bounds = [[0,0], [1200,1200]];
+let image1 = L.imageOverlay('maps/air_age_bigger.jpg', bounds);
 
-   let map = L.map('map', {
-        crs: L.CRS.Simple,
-        layers: [map1Group, map2Group]
-       });
+let image2 = L.imageOverlay('maps/flat_earth_xxl.jpg', bounds);
+
+let map1Group = L.layerGroup([image1]);
+let map2Group = L.layerGroup([image2]);
+
+let map = L.map('map', {
+     crs: L.CRS.Simple,
+     layers: [map1Group, map2Group],
+     drawControl: true
+    })
 // on localhost much bigger files work
-	map.fitBounds(bounds);
-	
-	var baseMaps = {
-         "Air Age Map": map1Group,
-         "flat earth xxl": map2Group,
-       };
-    L.control.layers(baseMaps).addTo(map);
+map.fitBounds(bounds);
 
-
+var baseMaps = {
+      "Air Age Map": map1Group,
+      "flat earth xxl": map2Group,
+    };
+ L.control.layers(baseMaps).addTo(map);
+ 
+ var drawnItems = new L.FeatureGroup();
+ map.addLayer(drawnItems);
+ var drawControl = new L.Control.Draw({
+     edit: {
+         featureGroup: drawnItems
+     }
+ });
+ map.addControl(drawControl);
